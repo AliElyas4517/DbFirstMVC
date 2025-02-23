fa778f5920b5eb56b77ee3edf7e3aad01ce94cdf
Ali Husnain Shah <76901747+ahshah322@users.noreply.github.com>
2025-02-23 11:29:39 +0500

    Updates added by Ali Husnain in HomeController

--- DbFirstMVC/Controllers/HomeController.cs
+++ DbFirstMVC/Controllers/HomeController.cs
@@ -8,21 +8,25 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
+    // Hi, Ali Ilyas
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
 
+    // Hi, Ali Ilyas
     public IActionResult Index()
     {
         return View();
     }
 
+    // Hi, Ali Ilyas
     public IActionResult Privacy()
     {
         return View();
     }
 
+    // Hi, Ali Ilyas
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
