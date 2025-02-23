1324d7a7ddd0482274111f5b79bae108602ac451
AliElyas4517 <aliilyas4517@gmail.com>
2025-02-23 11:55:54 +0500

    Modified Home Controller by Ali Ilyas

--- DbFirstMVC/Controllers/HomeController.cs
+++ DbFirstMVC/Controllers/HomeController.cs
@@ -8,27 +8,27 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    // Hi, Ali Ilyas
+    // Hi, Ali Bhai
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
 
-    // Hi, Ali Ilyas
+    // Hi, Ali Bhai
     public IActionResult Index()
     {
         Console.WriteLine("Modified Home Controller");
         return View();
     }
 
-    // Hi, Ali Ilyas
+    // Hi, Ali Bhai
     public IActionResult Privacy()
     {
         Console.WriteLine("Modified Home Controller");
         return View();
     }
 
-    // Hi, Ali Ilyas
+    // Hi, Ali Bhai
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
